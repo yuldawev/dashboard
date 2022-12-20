1eaba83afe589ccc3eaca0b2c0b1bacc4d62fd53
yuldawev <jasur.yuldawev01@gmail.com>
2022-12-20 19:53:36 +0500

    dashboard

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Oreders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+
+  {
+    productName: "LARVENDER KF102 Drone",
+    productNumber: "36378",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+
+  {
+    productName: "Ruko f11 Pro Drone",
+    productNumber: "49347",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Dro with Camera Drone",
+    productNumber: "96996",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "22821",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+
+  {
+    productName: "DJI Air 25",
+    productNumber: "81475",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Lozenge Drone",
+    productNumber: "00482",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
